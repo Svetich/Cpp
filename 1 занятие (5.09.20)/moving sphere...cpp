7ccbbb3d4a5ba1c0c751fa7f28a412d4ba769522
Svetich <aksenova.sa@phystech.edu>
2020-09-09 20:26:23 +0300

    Moving Sphere (version 1)

---
+++ 1 занятие (5.09.20)/moving sphere...cpp
@@ -0,0 +1,54 @@
+#include "TXLib.h"
+
+void drawSphere(int x, int y, int R, int N, int red, int green, int blue )
+{
+    for(int i = 0; i < N; i ++)
+        {
+            txSetFillColor( RGB(i * red / N, i * green / N, i * blue / N ));
+            txSetColor( RGB(i * red / N, i * green / N, i * blue / N) );
+            txCircle(x, y, R - R * i / N);
+        }
+}
+
+
+
+int main()
+{
+    const float dt = 1;
+
+    int R = 100;
+    int N = 1000;
+    int x = 100;
+    int y = 100;
+
+    int red = 0;
+    int green = 0;
+    int blue = 128;
+
+    float Vx = 10;
+    float Vy = 10;
+
+    txCreateWindow(800, 600);
+
+    while (true)
+    {
+        txClear();
+        txBegin();
+        drawSphere(x, y, R, N, 0, 0, 128 );
+        txEnd();
+
+        x = x + Vx * dt;
+        y = y + Vy * dt;
+
+        if ((x >= 800 - R) || (x <= R))
+        {
+            Vx = -Vx;
+        }
+        if ((y >= 600 - R) || (y <= R))
+        {
+            Vy = -Vy;
+        }
+    }
+
+    return 0;
+}
